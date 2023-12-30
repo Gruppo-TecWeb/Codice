6d1e83fe84fd320430adf0a1ccc58db6c9ba0931
alessandro bressan <bressan816@gmail.com>
2023-12-30 18:48:37 +0100

    battle1.1

--- battle.php
+++ battle.php
@@ -3,15 +3,16 @@
 namespace Utilities;
 require_once("utilities/utilities.php");
 
-$eventiHTML = file_get_contents("template/pagina-template.html");
+$pagTemplate = file_get_contents("template/pagina-template.html");
+
 
 $title = 'Battle &minus; Fungo';
 $pageId = basename(__FILE__, '.php');
-$description = '';
-$keywords = '';
+$description = "Introduzione regole e modalità freestyle per neofiti dell'argomento";
+$keywords = 'Modalità freestyle, Chyper, Kickback, Royal rumble';
 $menu = get_menu($pageId);
 $breadcrumbs = get_breadcrumbs($pageId);
-$content = '';
+$content = file_get_contents("battle.html");
 $onload = '';
 
-echo replace_in_page($eventiHTML, $title, $description, $keywords, $pageId, $menu, $breadcrumbs, $content, $onload);
+echo replace_in_page($pagTemplate, $title, $description, $keywords, $pageId, $menu, $breadcrumbs, $content, $onload);
