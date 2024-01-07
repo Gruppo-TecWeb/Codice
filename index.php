eeac9f1d96d6dfd1931c4bc89866bec4e69fce1d
Sebastiano Lewental <lewental.s@gmail.com>
2024-01-07 23:16:43 +0100

    inizio pagina home e modifiche al look del menu

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ namespace Utilities;
 include "utilities/utilities.php";
 
 $indexHTML = file_get_contents("template/pagina-template.html");
+$content = file_get_contents("template/index-content.html");
 
 $title = 'Fungo';
 $pageId = basename(__FILE__, '.php');
@@ -11,10 +12,6 @@ $description = '';
 $keywords = '';
 $menu = get_menu($pageId);
 $breadcrumbs = get_breadcrumbs($pageId);
-$content = '';
 $onload = '';
 
 echo replace_in_page($indexHTML, $title, $description, $keywords, $pageId, $menu, $breadcrumbs, $content, $onload);
-
-
-//carosello eventi
