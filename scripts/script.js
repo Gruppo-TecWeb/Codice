750c4ec37d5df4f8138a007d4c2df26a64b6652b
Sebastiano Lewental <lewental.s@gmail.com>
2024-01-09 13:01:01 +0100

    apertura menu in script.js ora blocca overflow quando aperto, tolta .limitedWidth su main in html

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,5 @@ function toggleMenu() {
     var nav = document.getElementById("menu");
     navOpened = !navOpened;
     nav.setAttribute("opened", navOpened);
+    document.body.style.overflowY = navOpened ? "hidden" : "unset";
 }
