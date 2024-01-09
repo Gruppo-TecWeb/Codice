353621dd48a28fd023e7dbab7ee5c1ccef71830d
Sebastiano Lewental <lewental.s@gmail.com>
2024-01-09 14:04:26 +0100

    cambio attributo opened a data-opened in #menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ var navOpened = false;
 function toggleMenu() {
     var nav = document.getElementById("menu");
     navOpened = !navOpened;
-    nav.setAttribute("opened", navOpened);
+    nav.setAttribute("data-opened", navOpened);
     document.body.style.overflowY = navOpened ? "hidden" : "unset";
 }
