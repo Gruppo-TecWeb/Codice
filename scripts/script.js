01ae0a84933313ef3c7b0078352eef82c7221c6c
Sebastiano Lewental <lewental.s@gmail.com>
2024-01-10 18:36:50 +0100

    restyle menu mobile, aggiunta animazione breadcrumbs mobile, style degli input, stile generale

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ var navOpened = false;
 
 function toggleMenu() {
     var nav = document.getElementById("menu");
+    var bcContainer = document.getElementById("breadcrumbs-container");
     navOpened = !navOpened;
     nav.setAttribute("data-opened", navOpened);
+    bcContainer.style.minWidth = navOpened ? "90%" : "";
     document.body.style.overflowY = navOpened ? "hidden" : "unset";
 }
--- utilities/utilities.php
+++ utilities/utilities.php
@@ -6,7 +6,7 @@ const pages_array = [
     'index'         => ['href' => 'index.php',          'anchor' => 'Home',         'lang' => 'en', 'menuOrder' => 1, 'parentId' => ''],
     'eventi'        => ['href' => 'eventi.php',         'anchor' => 'Eventi',       'lang' => '',   'menuOrder' => 2, 'parentId' => 'index'],
     'classifiche'   => ['href' => 'classifiche.php',    'anchor' => 'Classifiche',  'lang' => '',   'menuOrder' => 3, 'parentId' => 'index'],
-    'battle'        => ['href' => 'battle.php',         'anchor' => 'Tipi di <psan lang="en">Battle<span>',       'lang' => '', 'menuOrder' => 4, 'parentId' => 'index'],
+    'battle'        => ['href' => 'battle.php',         'anchor' => 'Tipi di <span lang="en">Battle<span>',       'lang' => '', 'menuOrder' => 4, 'parentId' => 'index'],
     'chi-siamo'     => ['href' => 'chi-siamo.php',      'anchor' => 'Chi siamo',    'lang' => '',   'menuOrder' => 5, 'parentId' => 'index']
 ];
 
@@ -47,7 +47,7 @@ function get_breadcrumbs($pageId) {
     $parent = $page['parentId'] != '' ? pages_array[$page['parentId']] : '';
     while ($parent != '') {
         $lang_tag = $parent['lang'] ? ' lang="' . $parent['lang'] . '"' : '';
-        $breadcrumbs .= '<a href="' . $parent['href'] . '"' . $lang_tag . '>' . $parent['anchor'] . '</a> &rsaquo;&rsaquo; ';
+        $breadcrumbs .= '<a href="' . $parent['href'] . '"' . $lang_tag . '>' . $parent['anchor'] . '</a> <span aria-hidden="true">&rsaquo;&rsaquo; </span>';
         $parent = $parent['parentId'] != '' ? pages_array[$parent['parentId']] : '';
     }
     $lang_tag = $page['lang'] ? ' lang="' . $page['lang'] . '"' : '';
