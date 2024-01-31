0859a4c198e2936e6194aa0cd349aa3c1668028e
Lewental Digital <lewental.s@gmail.com>
2024-01-31 12:05:35 +0100

    cambio nome home-template.html in home.html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once "utilities/utilities.php";
 session_start();
 
 $paginaHTML = file_get_contents("template/pagina-template.html");
-$content = file_get_contents("template/home-template.html");
+$content = file_get_contents("template/home.html");
 
 $title = 'Fungo';
 $pageId = basename(__FILE__, '.php');
