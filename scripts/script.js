4424a134fe2b6cb95282d187d5284367f6044dd7
Riccardo <riccardozaupa02@gmail.com>
2024-02-13 10:19:35 +0100

    Rimossa funzione init_eventi, ora inutile con il nuovo form

--- scripts/script.js
+++ scripts/script.js
@@ -9,28 +9,6 @@ function toggleMenu() {
     document.body.setAttribute("data-menu-open", navOpened);
 }
 
-function init_eventi() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var filtro = urlParams.get('filtro');
-    if (filtro === 'data') {
-        document.getElementById('data').classList.add('selected');
-        document.getElementById('link-passati').classList.remove('selected');
-        document.getElementById('link-tutti').classList.remove('selected');
-        document.getElementById('link-prossimi').classList.remove('selected');
-    } else {
-        // Altrimenti, aggiungi la classe "selected" al link corrispondente
-        document.getElementById('data-container').classList.remove('selected');
-        if (filtro === 'passati') {
-            document.getElementById('link-passati').classList.add('selected');
-        } else if (filtro === 'tutti') {
-            document.getElementById('link-tutti').classList.add('selected');
-        } else {
-            document.getElementById('link-prossimi').classList.add('selected');
-        }
-    }
-}
-
-
 function setIframe(battle){
     
     title=new Array(
