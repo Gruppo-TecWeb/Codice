3c01124a8d42a911a0d859f97e61b3e5f71746c6
T-Brex <bressan816@gmail.com>
2024-03-01 01:25:24 +0100

    ho sbagliato mergiando il js, ma ho risolto!

--- scripts/script.js
+++ scripts/script.js
@@ -44,151 +44,160 @@ function init_evento() {
  * PAGINA BEATS
 */
 
-function setAudioDuration() {
-    span = document.getElementsByClassName("durata");
-    audios = document.getElementsByClassName("audioBeats");
+function setAudioDuration(){
+    
+    audios=document.getElementsByClassName("audioBeats");
+ 
+    for(let i=0;i<audios.length;i++){
+        
+        durate=document.getElementsByClassName("durata")[i];
+        readDurata=document.getElementsByClassName("readDurata")[i];
+        
+        playerJump=document.getElementsByClassName("beat")[i].getElementsByTagName("a")[0].getElementsByTagName("span")[0];
+        playerJump.setAttribute("aria-hidden","true");
+        
+        audios[i].setAttribute("tabindex","-1");
+        audios[i].setAttribute("data-java","true");
 
-
-    for (let i = 0; i < span.length; i++) {
-        playerJump = document.getElementsByClassName("beat")[i].getElementsByTagName("a")[0].getElementsByTagName("span")[0];
-        playerJump.setAttribute("aria-hidden", "true");
-
-        audios[i].setAttribute("tabindex", "-1");
-        audios[i].setAttribute("data-java", "true");
-        durata = Math.floor(audios[i].duration / 60) + ":" + Math.floor(audios[i].duration % 60);
-        minuti = durata.slice(0, durata.indexOf(":"));
-        secondi = durata.slice(durata.indexOf(":") + 1);
-        if (minuti == 1) {
-            if (secondi.length == 1) {
-                span[i].innerHTML = "<time aria-hidden='true' datatime=PT" + minuti + "M" + secondi + "S>" + minuti + ":" + "0" + secondi + "</time>" + "<span class='navigationHelp'>" + minuti + "minuto e " + secondi + "secondi" + "</span>";
-            } else {
-                span[i].innerHTML = "<time aria-hidden='true' datatime=PT" + minuti + "M" + secondi + "S>" + minuti + ":" + secondi + "</time>" + "<span class='navigationHelp'>" + minuti + " minuto e " + secondi + " secondi" + "</span>";
+        durata=Math.floor(audios[i].duration/60) + ":" + Math.floor(audios[i].duration%60);
+        minuti=durata.slice(0,durata.indexOf(":"));
+        secondi=durata.slice(durata.indexOf(":")+1);
+        if(minuti==1){
+            if(secondi.length==1){
+                //durate.setAttribute("datatime","PT" + minuti + "M" + secondi + "S");
+                durate.innerHTML =minuti + ":" + "0" + secondi;
+                readDurata.innerHTML=minuti+" minuto e "+secondi+" secondi";
+            }else{
+                //durate.setAttribute("datatime","PT" + minuti + "M" + secondi + "S");
+                durate.innerHTML =minuti + ":" + secondi;
+                readDurata.innerHTML=minuti+" minuto e "+secondi+" secondi";
             }
-        } else {
-            if (secondi.length == 1) {
-                span[i].innerHTML = "<time aria-hidden='true' datatime=PT" + minuti + "M" + secondi + "S>" + minuti + ":" + "0" + secondi + "</time>" + "<span class='navigationHelp'>" + minuti + "minuti e " + secondi + "secondi" + "</span>";
-            } else {
-                span[i].innerHTML = "<time aria-hidden='true' datatime=PT" + minuti + "M" + secondi + "S>" + minuti + ":" + secondi + "</time>" + "<span class='navigationHelp'>" + minuti + " minuti e " + secondi + " secondi" + "</span>";
+        }else{
+            if(secondi.length==1){
+                //durate.setAttribute("datatime","PT" + minuti + "M" + secondi + "S");
+                durate.innerHTML =minuti + ":" + "0" + secondi;
+                readDurata.innerHTML=minuti+" minuti e "+secondi+" secondi";
+            }else{
+                //durate.setAttribute("datatime","PT" + minuti + "M" + secondi + "S");
+                durate.innerHTML =minuti + ":" + secondi;
+                readDurata.innerHTML=minuti+" minuti e "+secondi+" secondi";
+                //durata[i].innerHTML = "<time aria-hidden='true' datatime=PT" + minuti + "M" + secondi + "S>" + minuti + ":" + secondi + "</time>"+ "<span class='navigationHelp'>"+minuti+" minuti e "+secondi+" secondi"+"</span>"; 
             }
         }
     }
 }
 
 
-var autoNext = false;
-
-function playerAudio(nomeBase) {
-
+var autoNext=false;
+function playerAudio(nomeBase){
+    
     //variabili varie
-    percorso = "assets/media/basi/";
+    percorso="assets/media/basi/";
     audio = document.getElementById("audio");
-    audioContainer = document.getElementById("audio_container");
+    audioContainer=document.getElementById("audio_container");
     h3 = audioContainer.getElementsByTagName("h3")[0];
-    newTitle = nomeBase.slice(0, -4).replaceAll("-", " ");
+    newTitle = nomeBase.slice(0,-4).replaceAll("-"," ");
     beats = document.getElementsByClassName("beat")
-    for (let i = 0; i < beats.length; i++) {
-        console.log(playerJump);
-        if (beats[i].getElementsByTagName("button")[0].getAttribute("data-title-beat") == nomeBase.slice(0, -4)) {
-            pressedButton = beats[i].getElementsByTagName("button")[0];
-            audioJump = beats[i].getElementsByTagName("a")[0];
-            audioJump.setAttribute("tabindex", "0");
+    for(let i=0;i<beats.length;i++){
+        if(beats[i].getElementsByTagName("button")[0].getAttribute("data-title-beat")==nomeBase.slice(0,-4)){
+            pressedButton=beats[i].getElementsByTagName("button")[0];
+            audioJump=beats[i].getElementsByTagName("a")[0];
+            audioJump.setAttribute("tabindex","0");
 
-            playerJump = beats[i].getElementsByTagName("a")[0].getElementsByTagName("span")[0];
-            playerJump.setAttribute("aria-hidden", "false");
-            console.log(i + playerJump);
+            playerJump=beats[i].getElementsByTagName("a")[0].getElementsByTagName("span")[0];
+            playerJump.setAttribute("aria-hidden","false");
         }
     }
+    
 
-
-    if (h3.innerHTML == newTitle) {
-        if (pressedButton.title.slice(0, 10) == "Interrompi") {
+    if(h3.innerHTML==newTitle){
+        if(pressedButton.title.slice(0,10)=="Interrompi"){
             //console.log("pause");
             audio.pause();
-            pressedButton.setAttribute("data-isPlaying", "false")
-            pressedButton.title = "Riproduci " + newTitle;
+            pressedButton.setAttribute("data-isPlaying","false")
+            pressedButton.title="Riproduci " + newTitle;
 
+           
 
-
-
-        } else {
+        }else{
             audio.play();
-            pressedButton.setAttribute("data-isPlaying", "true")
-            pressedButton.title = "Interrompi " + newTitle;
-
-
-        }
-    } else {
+            pressedButton.setAttribute("data-isPlaying","true")
+            pressedButton.title="Interrompi " + newTitle;
+            
+            
+        }    
+    }else{
         newBeat(nomeBase);
-
+        
     }
-
+    
     //bottone riproduzione automatica
     autoPlay(nomeBase);
 }
 
-function newBeat(nomeBase) {
+function newBeat(nomeBase){
     //cambia gli statement dell'audio che era in riproduzione precedentemente
-    for (let i = 0; i < beats.length; i++) {
-        buttonPP = beats[i].getElementsByTagName("button")[0];
-        audioJump = beats[i].getElementsByTagName("a")[0];
-        playerJump.setAttribute("aria-hidden", "false");
-
-        if (buttonPP.title.substr(0, 10) == "Interrompi") {
-            audioJump.setAttribute("tabindex", "-1");
-
-            buttonPP.setAttribute("data-isPlaying", "false")
-            buttonPP.title = "Riproduci " + buttonPP.getAttribute("data-title-beat");
-        }
+    for(let i=0;i<beats.length;i++){
+        buttonPP=beats[i].getElementsByTagName("button")[0];
+        audioJump=beats[i].getElementsByTagName("a")[0];
+        playerJump.setAttribute("aria-hidden","false");
+        
+        if(buttonPP.title.substr(0,10)=="Interrompi"){
+            audioJump.setAttribute("tabindex","-1");
+            
+            buttonPP.setAttribute("data-isPlaying","false")
+            buttonPP.title="Riproduci " + buttonPP.getAttribute("data-title-beat");
+        } 
     }
     //settaggio title bottone e player audio
-    pressedButton.setAttribute("data-isPlaying", "true")
-    h3.innerHTML = newTitle;
-    pressedButton.title = "Interrompi " + newTitle;
-
+    pressedButton.setAttribute("data-isPlaying","true")
+    h3.innerHTML=newTitle; 
+    pressedButton.title="Interrompi " + newTitle;
+    
     //settaggio audio
     audio.setAttribute("autoplay", "true");
-    audio.src = percorso + nomeBase;
+    audio.src = percorso + nomeBase; 
 }
 
 
-function autoPlay(nomeBase) {
+function autoPlay(nomeBase){
     document.getElementById("autoNext").onclick = function() {
         autoNext = !autoNext;
         console.log(autoNext);
         document.getElementById("autoNext").setAttribute("aria-pressed", autoNext);
     }
     audio.onended = function() {
-        if (autoNext) {
-
+        if(autoNext){
+            
             audio.setAttribute("autoplay", "true");
-            nextAudio(nomeBase);
-        } else {
-
+                nextAudio(nomeBase);
+        }else{
+            
             audio.setAttribute("autoplay", "false");
 
+            }
         }
-    }
 }
 
-function nextAudio(nomeBase) {
-    newTitle = nomeBase.slice(0, -4).replaceAll("-", " ");
+function nextAudio(nomeBase) {  
+    newTitle = nomeBase.slice(0,-4).replaceAll("-"," ");
     beats = document.getElementsByClassName("beat")
-    for (let i = 0; i < beats.length; i++) {
-        if (beats[i].getElementsByTagName("button")[0].getAttribute("data-title-beat") == nomeBase.slice(0, -4)) {
-            pressedButton = beats[i].getElementsByTagName("button")[0];
+    for(let i=0;i<beats.length;i++){
+        if(beats[i].getElementsByTagName("button")[0].getAttribute("data-title-beat")==nomeBase.slice(0,-4)){
+            pressedButton=beats[i].getElementsByTagName("button")[0];
         }
     }
 
     for (let i = 0; i < beats.length; i++) {
         bottone = beats[i].getElementsByTagName("button")[0]
-        if (bottone.getAttribute("data-title-beat") == nomeBase.slice(0, -4)) {
-            let next = beats[i + 1];
+        if(bottone.getAttribute("data-title-beat") == nomeBase.slice(0,-4)){
+            let next = beats[i+1];
             if (next) {
                 let nextButton = next.getElementsByTagName("button");
-                newTitle = nextButton[0].getAttribute("data-title-beat") + ".mp3";
+                newTitle=nextButton[0].getAttribute("data-title-beat")+".mp3";
                 playerAudio(newTitle);
             }
-            break;
+            break;  
         }
     }
 }
