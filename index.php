c58014f7eb0167ec827c0b38a9e6876afa75fe4a
Lewental Digital <lewental.s@gmail.com>
2024-05-16 11:34:06 +0200

    fixata array_values usata male

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if ($connectionOk) {
     if (count($listaEventi) > 0) { // se ho ottenuto eventi
         $eventoId = $listaEventi[0]['Id'];
         $evento = $connection->get_evento($eventoId);
-        [$titolo, $descrizione, $data, $ora, $luogo, $locandina, $tipoEvento] = array_values($evento);
+        [$tipoEvento, $titolo, $descrizione, $data, $ora, $luogo, $locandina] = array_values($evento);
 
         $contentEvento = multi_replace(replace_content_between_markers($eventoHome, [
             'intestazione' => $headingEvento
